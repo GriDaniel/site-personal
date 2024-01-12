da86cee9f56ab0e82787ee5062c3cf112d429d97
GriDaniel <dan.grioliveira@gmail.com>
2024-01-12 06:23:24 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/site-personal",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['/site-personal/assets/index-bdcb863d.js'],
+      
+    },
+  },
 })
