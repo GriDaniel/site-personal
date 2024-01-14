4a83f89c3e003e45594bff624d46ce491c78f429
GriDaniel <dan.grioliveira@gmail.com>
2024-01-14 17:46:56 -0500

    update js for navbar hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 function Navbar({ darkMode, toggleDarkMode }) {
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const handleMobileMenuToggle = () => {
+        setMobileMenuOpen(!isMobileMenuOpen);
+    };
     return (
         <>
             <nav className="fixed w-screen flex justify-center  items-center z-20 top-0 left-0">
@@ -12,7 +17,7 @@ function Navbar({ darkMode, toggleDarkMode }) {
                         <img className="h-full w-full" src='https://github.com/GriDaniel/site-personal/blob/main/public/images/personal-logol.png?raw=true'></img>
                     </div>
                     <div className="flex md:order-2 md:w-[1000px] w-[75px] items-center justify-end-reverse">
-                        <button data-collapse-toggle="navbar-sticky" type="button" class="ml-[10px] inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:scale-[130%]" aria-controls="navbar-sticky" aria-expanded="false">
+                        <button data-collapse-toggle="navbar-sticky" type="button" class="ml-[10px] inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:scale-[130%]" aria-controls="navbar-sticky" aria-expanded="false" onTouchStart={handleMobileMenuToggle}>
                             <span className="sr-only">Open main menu</span>
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
