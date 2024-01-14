965a776c566fb1c1c1ab90db00319a2105155414
GriDaniel <dan.grioliveira@gmail.com>
2024-01-14 00:22:38 -0500

    update config

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar({ darkMode, toggleDarkMode }) {
                         <img className="h-full w-full" src='https://github.com/GriDaniel/site-personal/blob/main/public/images/name-logo.png?raw=true'></img>
                     </div>
                     <div className='block dark:hidden w-[100px] h-full relative'>
-                        <img className="h-full w-full" src='https://github.com/GriDaniel/site-personal/blob/main/public/images/name-logol.png?raw=true'></img>
+                        <img className="h-full w-full" src='https://github.com/GriDaniel/site-personal/blob/main/public/images/personal-logol.png?raw=true'></img>
                     </div>
                     <div className="flex md:order-2 md:w-[1000px] w-[75px] items-center justify-end-reverse">
                         <button data-collapse-toggle="navbar-sticky" type="button" class="ml-[10px] inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:scale-[130%]" aria-controls="navbar-sticky" aria-expanded="false">
