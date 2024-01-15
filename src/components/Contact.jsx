c79aca6763b657d16b0d005cb093aa663660964c
GriDaniel <dan.grioliveira@gmail.com>
2024-01-15 16:49:53 -0500

    update mailbox href

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div  id="contact" className='min-w-screen overflow-x-hidden overflow-hidden flex justify-center items-center dark:bg-black bg-[#F4F4F4]'>
-            <div  className='md:pt-[50px] pt-[100px] pb-[25px] min-w-screen min-h-[300px] overflow-x-hidden overflow-hidden flex dark:bg-black flex-col bg-[#F4F4F4]'>
+        <div id="contact" className='min-w-screen overflow-x-hidden overflow-hidden flex justify-center items-center dark:bg-black bg-[#F4F4F4]'>
+            <div className='md:pt-[50px] pt-[100px] pb-[25px] min-w-screen min-h-[300px] overflow-x-hidden overflow-hidden flex dark:bg-black flex-col bg-[#F4F4F4]'>
                 <div className='h-[200px] w-full  flex items-start justify-center'>
-                    <h1  className='dark:text-white text-black font-satoshi text-5xl'>Stay in touch.</h1>
+                    <h1 className='dark:text-white text-black font-satoshi text-5xl'>Stay in touch.</h1>
 
                 </div>
                 <div className='max:w-[1920px]  w-screen min-h-[250px]  flex items-center justify-center md:flex-row flex-col'>
@@ -31,7 +31,7 @@ function Contact() {
                                     <a href='https://github.com/GriDaniel' target='blank' className='w-[40px] h-[40px] dark:hidden block'>
                                         <img className='hover:scale-105 rounded-full  w-full h-full' src="https://github.com/GriDaniel/site-personal/blob/main/public/images/git-dark.png?raw=true" />
                                     </a>
-                                    <a href="">
+                                    <a href="mailto:dan.grioliveira@gmail.com">
                                         <svg className='dark:block hover:scale-105 hidden w-[40px] h-[40px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" id="mail"><path fill="#ffffff" d="M16,14.81,28.78,6.6A3,3,0,0,0,27,6H5a3,3,0,0,0-1.78.6Z" class="color231f20 svgShape"></path><path fill="#ffffff" d="M16.54,16.84h0l-.17.08-.08,0A1,1,0,0,1,16,17h0a1,1,0,0,1-.25,0l-.08,0-.17-.08h0L2.1,8.26A3,3,0,0,0,2,9V23a3,3,0,0,0,3,3H27a3,3,0,0,0,3-3V9a3,3,0,0,0-.1-.74Z" class="color231f20 svgShape"></path></svg>
                                         <svg className='dark:hidden hover:scale-105 block w-[40px] h-[40px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" id="mail"><path fill="#000000" d="M16,14.81,28.78,6.6A3,3,0,0,0,27,6H5a3,3,0,0,0-1.78.6Z" class="color231f20 svgShape"></path><path fill="#000000" d="M16.54,16.84h0l-.17.08-.08,0A1,1,0,0,1,16,17h0a1,1,0,0,1-.25,0l-.08,0-.17-.08h0L2.1,8.26A3,3,0,0,0,2,9V23a3,3,0,0,0,3,3H27a3,3,0,0,0,3-3V9a3,3,0,0,0-.1-.74Z" class="color231f20 svgShape"></path></svg>
                                     </a>
